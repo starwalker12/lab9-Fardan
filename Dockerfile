c198967b35985463c8bd4bbbc96cbc74d9562eb6
Fardan Aatir <fardan.aatir@outlook.com>
2025-11-24 17:25:27 +0500

    Final

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,4 @@ RUN npm install
 COPY . .
 RUN npm run build
 EXPOSE 3000
-CMD ["npm","start"]
-
-
+CMD ["npm", "start"]
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,37 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: { unoptimized: true },
+  output: "export",                     // ⬅️ REQUIRED for static export
+  images: {
+    unoptimized: true,                  // ⬅️ REQUIRED for next export
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
+
